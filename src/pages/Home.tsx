90cd17d4ba8265f4ae6ff000b148d649aca6609e
bufi.hofa <hoapri123@gmail.com>
2025-07-23 22:09:56 +0700

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,6 @@ import './Home.css'
 const Home: React.FC = () => {
   return (
     <div>
-      <h2>Home Page</h2>
-      <p>Welcome to the home page of Idle Nationsdfghjádasdasdasdasdasdasdasdasdsadsaklasdfghjklzxcvbnm,qwertyuiopqwertyuiop!</p>
       <p>Welcome to the home page of Idle Nation!</p>
       <p>Welcome to the home page of Idle Nation!</p>
       <p>Welcome to the home page of Idle Nation!</p>
